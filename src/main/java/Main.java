7fc94fa2d3c73b6fe31fcfdcce4ecb559acfc20e
Taylor Belcher <30426680+hotdogontology@users.noreply.github.com>
2026-04-24 12:56:59 -0400

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
+// you will need to create other class files as necessary
+// what other classes files you make will depend on your game and what you choose to do 
 public class Main{
-
   public static void main(String[] args){
-    // run you game through the main method here
+    // run your game through the main method here
   }
-
 }
